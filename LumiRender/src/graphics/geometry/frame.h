a17488dc21acc8c04d5098554c55f5cd1afd268a
Zero <249002031@qq.com>
2021-02-07 22:55:01 +0800

    Update frame.h

--- LumiRender/src/graphics/geometry/frame.h
+++ LumiRender/src/graphics/geometry/frame.h
@@ -29,9 +29,70 @@ namespace luminous {
                 return s * local_v.x + t * local_v.y + n * local_v.z;
             }
 
-            XPU scalar_t cos_theta_2(vector_t &v) {
+            XPU static scalar_t cos_theta_2(vector_t v) {
                 return sqr(v.z);
             }
+
+            XPU static scalar_t cos_theta(const vector_t v) {
+                return v.z;
+            }
+
+            XPU static scalar_t sin_theta_2(const vector_t v) {
+                return 1.0f - cos_theta_2(v);
+            }
+
+            XPU static scalar_t sin_theta(const vector_t v) {
+                scalar_t temp = sin_theta_2(v);
+                if (temp <= 0.0f)
+                    return 0.0f;
+                return sqrt(temp);
+            }
+
+            XPU static scalar_t tan_theta(const vector_t &v) {
+                scalar_t sin_theta2 = 1 - cos_theta_2(v);
+                if (sin_theta2 <= 0.0f)
+                    return 0.0f;
+                return std::sqrt(sin_theta2) / cos_theta(v);
+            }
+
+            XPU static scalar_t tan_theta_2(const vector_t &v) {
+                scalar_t cos_theta2 = cos_theta_2(v);
+                scalar_t sin_theta2 = 1 - cos_theta2;
+                if (sin_theta2 <= 0.0f)
+                    return 0.0f;
+                return sin_theta2 / cos_theta2;
+            }
+
+            XPU static scalar_t sin_phi(const vector_t &v) {
+                scalar_t sinTheta = sin_theta(v);
+                if (sinTheta == (scalar_t)0)
+                    return 1;
+                return clamp(v.y / sinTheta, (scalar_t) -1, (scalar_t) 1);
+            }
+
+            XPU static scalar_t cos_phi(const vector_t &v) {
+                scalar_t sinTheta = sin_theta(v);
+                if (sinTheta == (scalar_t)0)
+                    return 1;
+                return clamp(v.x / sinTheta, (scalar_t) -1, (scalar_t) 1);
+            }
+
+            XPU static scalar_t sin_phi_2(const vector_t &v) {
+                return clamp(sqr(v.y) / sin_theta_2(v), (scalar_t) 0, (scalar_t) 1);
+            }
+
+            XPU static scalar_t cos_phi_2(const vector_t &v) {
+                return clamp(sqr(v.x) / sin_theta_2(v), (scalar_t) 0, (scalar_t) 1);
+            }
+
+            XPU bool operator==(const TFrame &frame) const {
+                return frame.s == s && frame.t == t && frame.n == n;
+            }
+
+            XPU bool operator!=(const TFrame &frame) const {
+                return !operator==(frame);
+            }
+
 
         };
 
