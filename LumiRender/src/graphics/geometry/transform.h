5cc4573c3fc3970bed6223deea5607534d95e8c2
Zero <249002031@qq.com>
2021-02-08 14:17:31 +0800

    Update transform.h

--- LumiRender/src/graphics/geometry/transform.h
+++ LumiRender/src/graphics/geometry/transform.h
@@ -8,20 +8,17 @@
 namespace luminous {
     inline namespace geometry {
 
-        template<typename T>
-        struct TTransform {
-        public:
-            using scalar_t = T;
+        struct Transform {
         private:
-            Matrix4x4<T> _mat;
-            Matrix4x4<T> _inv_mat;
+            float4x4 _mat;
+            float4x4 _inv_mat;
 
         public:
-            XPU TTransform(Matrix4x4<T> mat = Matrix4x4<T>(1))
+            XPU Transform(float4x4 mat = float4x4(1))
                 : _mat(mat),
                 _inv_mat(inverse(mat)) {}
 
-            XPU TTransform(Matrix4x4<T> mat, Matrix4x4<T> inv)
+            XPU Transform(float4x4 mat, float4x4 inv)
                 : _mat(mat),
                 _inv_mat(inv) {}
 
@@ -30,36 +27,62 @@ namespace luminous {
             }
 
             XPU auto mat3x3() const {
-                return Matrix<T, 3>(_mat[0].vec3(), _mat[1].vec3(), _mat[2].vec3());
+                return make_float3x3(_mat);
             }
 
             XPU auto inv_mat3x3() const {
                 return inverse(mat3x3());
             }
 
-            XPU Vector<T, 3> apply_point(Vector<T, 3> point) {
-                Vector<T, 4> homo_point = Vector<T, 4>(point.x, point.y, point.z, (T)1);
+            XPU float3 apply_point(float3 point) {
+                float4 homo_point = make_float4(point, 1.f);
                 homo_point = _mat * homo_point;
-                return Vector<T, 3>(homo_point.x, homo_point.y, homo_point.z);
+                return make_float3(homo_point);
             }
 
-            XPU Vector<T, 3> apply_vector(Vector<T, 3> vec) {
+            XPU float3 apply_vector(float3 vec) {
                 return mat3x3() * vec;
             }
 
-            XPU Vector<T, 3> apply_normal(Vector<T, 3> normal) {
+            XPU float3 apply_normal(float3 normal) {
                 return transpose(inv_mat3x3()) * normal;
             }
 
-            TTransform operator*(const TTransform &t) const { 
-                return TTransform(m * t.mat4x4()); 
+            XPU Transform operator*(const Transform &t) const { 
+                return Transform(_mat * t.mat4x4()); 
             }
 
-            XPU static TTransform translate(Vector<T, 3> t) const {
-                // auto mat = 
+            XPU Transform inverse() const {
+                return Transform(_inv_mat, _mat);
+            }
+
+            XPU static Transform translation(float3 t) {
+                auto mat = make_float4x4(
+                        1.f, 0.f, 0.f, 0.f,
+                        0.f, 1.f, 0.f, 0.f,
+                        0.f, 0.f, 1.f, 0.f,
+                        t.x, t.y, t.z, 1.f);
+                auto inv = make_float4x4(
+                        1.0f, 0.0f, 0.0f, 0.0f,
+                        0.0f, 1.0f, 0.0f, 0.0f,
+                        0.0f, 0.0f, 1.0f, 0.0f,
+                        -t.x, -t.y, -t.z, 1.0f);
+                return Transform(mat, inv);
+            }
+
+            XPU static Transform scale(float3 s) {
+                auto mat = make_float4x4(
+                        s.x, 0.f, 0.f, 0.f,
+                        0.f, s.y, 0.f, 0.f,
+                        0.f, 0.f, s.z, 0.f,
+                        0.f, 0.f, 0.f, 1.f);
+                auto inv = make_float4x4(
+                        1/s.x, 0.0f, 0.0f, 0.0f,
+                        0.0f, 1/s.y, 0.0f, 0.0f,
+                        0.0f, 0.0f, 1/s.z, 0.0f,
+                        0.0f, 0.0f, 0.0f, 1.0f);
+                return Transform(mat, inv);
             }
         };
-
-        using Transform = TTransform<float>;
     }
 }
