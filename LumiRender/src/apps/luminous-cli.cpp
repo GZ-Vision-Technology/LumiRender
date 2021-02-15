628f788838b6e186b7760aa79e667821363560ee
Zero <249002031@qq.com>
2021-02-16 00:33:31 +0800

    add test scene

--- LumiRender/src/apps/luminous-cli.cpp
+++ LumiRender/src/apps/luminous-cli.cpp
@@ -2,3 +2,14 @@
 // Created by Zero on 2021/1/29.
 //
 
+#include <iostream>
+#include "core/context.h"
+
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+//    luminous::Context context{argc, argv};
+    cout << argv[0];
+    return 0;
+}
