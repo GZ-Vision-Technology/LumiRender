8da858e4272e1dafcd78e3e4e7d8f58e9627377c
Zero <249002031@qq.com>
2021-03-04 20:39:59 +0800

    Update sensor.h

--- LumiRender/src/render/include/sensor.h
+++ LumiRender/src/render/include/sensor.h
@@ -16,6 +16,26 @@ namespace luminous {
         class PinholeCamera;
         class PerspectiveCamera;
 
+        class CameraBase {
+        private:
+            float3 _position;
+            float _fovy;
+            float _yaw;
+            float _pitch;
+            float _velocity;
+        public:
+            CameraBase(float3 pos = make_float3(0), float _fovy = 0);
+
+            float3 position() const;
+
+            float yaw() const;
+
+            float pitch() const;
+
+            float fovy() const;
+
+            float velocity() const;
+        }
 
         using lstd::Variant;
         class SensorHandle : public Variant<PinholeCamera*, PerspectiveCamera*> {
