5107363eb7c95b78aae9e7c28db49dabd3938310
Zero <249002031@qq.com>
2021-03-26 08:57:27 +0800

    update cuda

--- LumiRender/src/gpu/shaders/optix_kernels.cu
+++ LumiRender/src/gpu/shaders/optix_kernels.cu
@@ -14,10 +14,23 @@ __constant__ luminous::LaunchParams params;
 GLOBAL __raygen__rg() {
     const uint3 idx = optixGetLaunchIndex();
     auto pFilm = luminous::make_float2(idx.x, idx.y);
-    auto camera = params.camera;
-    auto film = camera->film();
-//    printf("%s\n", film->name());
-    film->add_sample(pFilm, luminous::make_float3(1.f,0.2f,1.f), 1.f);
+//    auto camera = params.camera;
+//    auto film = camera->film();
+////    printf("%s\n", film->name());
+//    film->add_sample(pFilm, luminous::make_float3(1.f,0.2f,1.f), 1.f);
+
+    float3 o = make_float3(0,0,0);
+    float3 d = make_float3(0,0,1);
+
+    RadiancePRD prd;
+
+    traceRadiance(
+            params.traversable_handle,
+            o,
+            d,
+            0.01f,  // tmin       // TODO: smarter offset
+            1e16f,  // tmax
+            &prd );
 }
 
 GLOBAL __miss__radiance() {
@@ -29,7 +42,7 @@ GLOBAL __miss__shadow() {
 }
 
 GLOBAL __closesthit__radiance() {
-
+    printf("asdf\n");
 }
 
 GLOBAL __closesthit__occlusion() {
--- LumiRender/src/gpu/shaders/optix_kernels.h
+++ LumiRender/src/gpu/shaders/optix_kernels.h
@@ -11,6 +11,12 @@
 #include "render/sensors/shader_include.h"
 #include "gpu/framework/optix_params.h"
 
+struct RadiancePRD
+{
+    // TODO: move some state directly into payload registers?
+    int          pad;
+};
+
 static __forceinline__ __device__ void *unpackPointer(unsigned int i0, unsigned int i1) {
     const unsigned long long uptr = static_cast<unsigned long long>( i0 ) << 32 | i1;
     void *ptr = reinterpret_cast<void *>( uptr );
@@ -21,4 +27,30 @@ static __forceinline__ __device__ void packPointer(void *ptr, unsigned int &i0, 
     const unsigned long long uptr = reinterpret_cast<unsigned long long>( ptr );
     i0 = uptr >> 32;
     i1 = uptr & 0x00000000ffffffff;
+}
+
+static __forceinline__ __device__ void traceRadiance(
+        OptixTraversableHandle handle,
+        float3                 ray_origin,
+        float3                 ray_direction,
+        float                  tmin,
+        float                  tmax,
+        RadiancePRD*           prd
+) {
+    unsigned int u0, u1;
+    packPointer( prd, u0, u1 );
+    optixTrace(
+            handle,
+            ray_origin,
+            ray_direction,
+            tmin,
+            tmax,
+            0.0f,                // rayTime
+            OptixVisibilityMask( 1 ),
+            OPTIX_RAY_FLAG_NONE,
+            luminous::RayType::Radiance,        // SBT offset
+            luminous::RayType::Count,           // SBT stride
+            luminous::RayType::Radiance,        // missSBTIndex
+            u0, u1 );
+//    printf("sdaf-----------\n");
 }
