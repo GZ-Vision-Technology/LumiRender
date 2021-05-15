6ac31be6ac2685cd86634c1530bd0fa9d217d2fb
Zero <249002031@qq.com>
2021-05-16 01:00:01 +0800

    Update test_camera.cpp

--- LumiRender/src/tests/test_camera.cpp
+++ LumiRender/src/tests/test_camera.cpp
@@ -10,17 +10,17 @@ using namespace std;
 
 void test_sensor() {
     SensorConfig config;
-    config.type = "PinholeCamera";
+    config.set_full_type("PinholeCamera");
     config.fov_y = 90;
     config.velocity = 20;
 
     TransformConfig tc;
-    tc.type = "yaw_pitch";
+    tc.set_type("yaw_pitch");
 
     tc.yaw = 0;
     tc.pitch = 0;
     tc.position = make_float3(0,0,0);
-    config.film_config.type = "RGBFilm";
+    config.film_config.set_full_type("RGBFilm");
     config.film_config.resolution = make_uint2(500,500);
     tc.mat4x4 = make_float4x4(1);
     config.transform_config = tc;
@@ -32,7 +32,7 @@ void test_sensor() {
 
     auto mat = camera.camera_to_world_rotation().mat4x4();
 
-    tc.type = "matrix4x4";
+    tc.set_full_type("matrix4x4");
     tc.mat4x4 = mat;
 
 
