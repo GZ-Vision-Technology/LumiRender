11d9ac9d07d26facfcf65007316ce5eb31cab023
Zero <249002031@qq.com>
2021-06-19 20:08:23 +0800

    Update optixPathTracer.cpp

--- LumiRender/src/sdk_pt/optixPathTracer.cpp
+++ LumiRender/src/sdk_pt/optixPathTracer.cpp
@@ -143,142 +143,142 @@ struct PathTracerState
 const int32_t TRIANGLE_COUNT = 32;
 const int32_t MAT_COUNT      = 1;
 
-const static std::array<Vertex, TRIANGLE_COUNT* 3> g_vertices =
+const static std::array<luminous::float4, TRIANGLE_COUNT* 3> g_vertices =
 {  {
     // Floor  -- white lambert
-    {    0.0f,    0.0f,    0.0f, 0.0f },
-    {    0.0f,    0.0f,  559.2f, 0.0f },
-    {  556.0f,    0.0f,  559.2f, 0.0f },
-    {    0.0f,    0.0f,    0.0f, 0.0f },
-    {  556.0f,    0.0f,  559.2f, 0.0f },
-    {  556.0f,    0.0f,    0.0f, 0.0f },
+    luminous::float4(    0.0f,    0.0f,    0.0f, 0.0f ),
+    luminous::float4(    0.0f,    0.0f,  559.2f, 0.0f ),
+    luminous::float4(  556.0f,    0.0f,  559.2f, 0.0f ),
+    luminous::float4(    0.0f,    0.0f,    0.0f, 0.0f ),
+    luminous::float4(  556.0f,    0.0f,  559.2f, 0.0f ),
+    luminous::float4(  556.0f,    0.0f,    0.0f, 0.0f ),
 
     // Ceiling -- white lambert
-    {    0.0f,  548.8f,    0.0f, 0.0f },
-    {  556.0f,  548.8f,    0.0f, 0.0f },
-    {  556.0f,  548.8f,  559.2f, 0.0f },
+    luminous::float4(    0.0f,  548.8f,    0.0f, 0.0f ),
+    luminous::float4(  556.0f,  548.8f,    0.0f, 0.0f ),
+    luminous::float4(  556.0f,  548.8f,  559.2f, 0.0f ),
 
-    {    0.0f,  548.8f,    0.0f, 0.0f },
-    {  556.0f,  548.8f,  559.2f, 0.0f },
-    {    0.0f,  548.8f,  559.2f, 0.0f },
+    luminous::float4(    0.0f,  548.8f,    0.0f, 0.0f ),
+    luminous::float4(  556.0f,  548.8f,  559.2f, 0.0f ),
+    luminous::float4(    0.0f,  548.8f,  559.2f, 0.0f ),
 
     // Back wall -- white lambert
-    {    0.0f,    0.0f,  559.2f, 0.0f },
-    {    0.0f,  548.8f,  559.2f, 0.0f },
-    {  556.0f,  548.8f,  559.2f, 0.0f },
+    luminous::float4(    0.0f,    0.0f,  559.2f, 0.0f ),
+    luminous::float4(    0.0f,  548.8f,  559.2f, 0.0f ),
+    luminous::float4(  556.0f,  548.8f,  559.2f, 0.0f ),
 
-    {    0.0f,    0.0f,  559.2f, 0.0f },
-    {  556.0f,  548.8f,  559.2f, 0.0f },
-    {  556.0f,    0.0f,  559.2f, 0.0f },
+    luminous::float4(    0.0f,    0.0f,  559.2f, 0.0f ),
+    luminous::float4(  556.0f,  548.8f,  559.2f, 0.0f ),
+    luminous::float4(  556.0f,    0.0f,  559.2f, 0.0f ),
 
     // Right wall -- green lambert
-    {    0.0f,    0.0f,    0.0f, 0.0f },
-    {    0.0f,  548.8f,    0.0f, 0.0f },
-    {    0.0f,  548.8f,  559.2f, 0.0f },
+    luminous::float4(    0.0f,    0.0f,    0.0f, 0.0f ),
+    luminous::float4(    0.0f,  548.8f,    0.0f, 0.0f ),
+    luminous::float4(    0.0f,  548.8f,  559.2f, 0.0f ),
 
-    {    0.0f,    0.0f,    0.0f, 0.0f },
-    {    0.0f,  548.8f,  559.2f, 0.0f },
-    {    0.0f,    0.0f,  559.2f, 0.0f },
+    luminous::float4(    0.0f,    0.0f,    0.0f, 0.0f ),
+    luminous::float4(    0.0f,  548.8f,  559.2f, 0.0f ),
+    luminous::float4(    0.0f,    0.0f,  559.2f, 0.0f ),
 
     // Left wall -- red lambert
-    {  556.0f,    0.0f,    0.0f, 0.0f },
-    {  556.0f,    0.0f,  559.2f, 0.0f },
-    {  556.0f,  548.8f,  559.2f, 0.0f },
+    luminous::float4(  556.0f,    0.0f,    0.0f, 0.0f ),
+    luminous::float4(  556.0f,    0.0f,  559.2f, 0.0f ),
+    luminous::float4(  556.0f,  548.8f,  559.2f, 0.0f ),
 
-    {  556.0f,    0.0f,    0.0f, 0.0f },
-    {  556.0f,  548.8f,  559.2f, 0.0f },
-    {  556.0f,  548.8f,    0.0f, 0.0f },
+    luminous::float4(  556.0f,    0.0f,    0.0f, 0.0f ),
+    luminous::float4(  556.0f,  548.8f,  559.2f, 0.0f ),
+    luminous::float4(  556.0f,  548.8f,    0.0f, 0.0f ),
 
     // Short block -- white lambert
-    {  130.0f,  165.0f,   65.0f, 0.0f },
-    {   82.0f,  165.0f,  225.0f, 0.0f },
-    {  242.0f,  165.0f,  274.0f, 0.0f },
+    luminous::float4(  130.0f,  165.0f,   65.0f, 0.0f ),
+    luminous::float4(   82.0f,  165.0f,  225.0f, 0.0f ),
+    luminous::float4(  242.0f,  165.0f,  274.0f, 0.0f ),
 
-    {  130.0f,  165.0f,   65.0f, 0.0f },
-    {  242.0f,  165.0f,  274.0f, 0.0f },
-    {  290.0f,  165.0f,  114.0f, 0.0f },
+    luminous::float4(  130.0f,  165.0f,   65.0f, 0.0f ),
+    luminous::float4(  242.0f,  165.0f,  274.0f, 0.0f ),
+    luminous::float4(  290.0f,  165.0f,  114.0f, 0.0f ),
 
-    {  290.0f,    0.0f,  114.0f, 0.0f },
-    {  290.0f,  165.0f,  114.0f, 0.0f },
-    {  240.0f,  165.0f,  272.0f, 0.0f },
+    luminous::float4(  290.0f,    0.0f,  114.0f, 0.0f ),
+    luminous::float4(  290.0f,  165.0f,  114.0f, 0.0f ),
+    luminous::float4(  240.0f,  165.0f,  272.0f, 0.0f ),
 
-    {  290.0f,    0.0f,  114.0f, 0.0f },
-    {  240.0f,  165.0f,  272.0f, 0.0f },
-    {  240.0f,    0.0f,  272.0f, 0.0f },
+    luminous::float4(  290.0f,    0.0f,  114.0f, 0.0f ),
+    luminous::float4(  240.0f,  165.0f,  272.0f, 0.0f ),
+    luminous::float4(  240.0f,    0.0f,  272.0f, 0.0f ),
 
-    {  130.0f,    0.0f,   65.0f, 0.0f },
-    {  130.0f,  165.0f,   65.0f, 0.0f },
-    {  290.0f,  165.0f,  114.0f, 0.0f },
+    luminous::float4(  130.0f,    0.0f,   65.0f, 0.0f ),
+    luminous::float4(  130.0f,  165.0f,   65.0f, 0.0f ),
+    luminous::float4(  290.0f,  165.0f,  114.0f, 0.0f ),
 
-    {  130.0f,    0.0f,   65.0f, 0.0f },
-    {  290.0f,  165.0f,  114.0f, 0.0f },
-    {  290.0f,    0.0f,  114.0f, 0.0f },
+    luminous::float4(  130.0f,    0.0f,   65.0f, 0.0f ),
+    luminous::float4(  290.0f,  165.0f,  114.0f, 0.0f ),
+    luminous::float4(  290.0f,    0.0f,  114.0f, 0.0f ),
 
-    {   82.0f,    0.0f,  225.0f, 0.0f },
-    {   82.0f,  165.0f,  225.0f, 0.0f },
-    {  130.0f,  165.0f,   65.0f, 0.0f },
+    luminous::float4(   82.0f,    0.0f,  225.0f, 0.0f ),
+    luminous::float4(   82.0f,  165.0f,  225.0f, 0.0f ),
+    luminous::float4(  130.0f,  165.0f,   65.0f, 0.0f ),
 
-    {   82.0f,    0.0f,  225.0f, 0.0f },
-    {  130.0f,  165.0f,   65.0f, 0.0f },
-    {  130.0f,    0.0f,   65.0f, 0.0f },
+    luminous::float4(   82.0f,    0.0f,  225.0f, 0.0f ),
+    luminous::float4(  130.0f,  165.0f,   65.0f, 0.0f ),
+    luminous::float4(  130.0f,    0.0f,   65.0f, 0.0f ),
 
-    {  240.0f,    0.0f,  272.0f, 0.0f },
-    {  240.0f,  165.0f,  272.0f, 0.0f },
-    {   82.0f,  165.0f,  225.0f, 0.0f },
+    luminous::float4(  240.0f,    0.0f,  272.0f, 0.0f ),
+    luminous::float4(  240.0f,  165.0f,  272.0f, 0.0f ),
+    luminous::float4(   82.0f,  165.0f,  225.0f, 0.0f ),
 
-    {  240.0f,    0.0f,  272.0f, 0.0f },
-    {   82.0f,  165.0f,  225.0f, 0.0f },
-    {   82.0f,    0.0f,  225.0f, 0.0f },
+    luminous::float4(  240.0f,    0.0f,  272.0f, 0.0f ),
+    luminous::float4(   82.0f,  165.0f,  225.0f, 0.0f ),
+    luminous::float4(   82.0f,    0.0f,  225.0f, 0.0f ),
 
     // Tall block -- white lambert
-    {  423.0f,  330.0f,  247.0f, 0.0f },
-    {  265.0f,  330.0f,  296.0f, 0.0f },
-    {  314.0f,  330.0f,  455.0f, 0.0f },
+    luminous::float4(  423.0f,  330.0f,  247.0f, 0.0f ),
+    luminous::float4(  265.0f,  330.0f,  296.0f, 0.0f ),
+    luminous::float4(  314.0f,  330.0f,  455.0f, 0.0f ),
 
-    {  423.0f,  330.0f,  247.0f, 0.0f },
-    {  314.0f,  330.0f,  455.0f, 0.0f },
-    {  472.0f,  330.0f,  406.0f, 0.0f },
+    luminous::float4(  423.0f,  330.0f,  247.0f, 0.0f ),
+    luminous::float4(  314.0f,  330.0f,  455.0f, 0.0f ),
+    luminous::float4(  472.0f,  330.0f,  406.0f, 0.0f ),
 
-    {  423.0f,    0.0f,  247.0f, 0.0f },
-    {  423.0f,  330.0f,  247.0f, 0.0f },
-    {  472.0f,  330.0f,  406.0f, 0.0f },
+    luminous::float4(  423.0f,    0.0f,  247.0f, 0.0f ),
+    luminous::float4(  423.0f,  330.0f,  247.0f, 0.0f ),
+    luminous::float4(  472.0f,  330.0f,  406.0f, 0.0f ),
 
-    {  423.0f,    0.0f,  247.0f, 0.0f },
-    {  472.0f,  330.0f,  406.0f, 0.0f },
-    {  472.0f,    0.0f,  406.0f, 0.0f },
+    luminous::float4(  423.0f,    0.0f,  247.0f, 0.0f ),
+    luminous::float4(  472.0f,  330.0f,  406.0f, 0.0f ),
+    luminous::float4(  472.0f,    0.0f,  406.0f, 0.0f ),
 
-    {  472.0f,    0.0f,  406.0f, 0.0f },
-    {  472.0f,  330.0f,  406.0f, 0.0f },
-    {  314.0f,  330.0f,  456.0f, 0.0f },
+    luminous::float4(  472.0f,    0.0f,  406.0f, 0.0f ),
+    luminous::float4(  472.0f,  330.0f,  406.0f, 0.0f ),
+    luminous::float4(  314.0f,  330.0f,  456.0f, 0.0f ),
 
-    {  472.0f,    0.0f,  406.0f, 0.0f },
-    {  314.0f,  330.0f,  456.0f, 0.0f },
-    {  314.0f,    0.0f,  456.0f, 0.0f },
+    luminous::float4(  472.0f,    0.0f,  406.0f, 0.0f ),
+    luminous::float4(  314.0f,  330.0f,  456.0f, 0.0f ),
+    luminous::float4(  314.0f,    0.0f,  456.0f, 0.0f ),
 
-    {  314.0f,    0.0f,  456.0f, 0.0f },
-    {  314.0f,  330.0f,  456.0f, 0.0f },
-    {  265.0f,  330.0f,  296.0f, 0.0f },
+    luminous::float4(  314.0f,    0.0f,  456.0f, 0.0f ),
+    luminous::float4(  314.0f,  330.0f,  456.0f, 0.0f ),
+    luminous::float4(  265.0f,  330.0f,  296.0f, 0.0f ),
 
-    {  314.0f,    0.0f,  456.0f, 0.0f },
-    {  265.0f,  330.0f,  296.0f, 0.0f },
-    {  265.0f,    0.0f,  296.0f, 0.0f },
+    luminous::float4(  314.0f,    0.0f,  456.0f, 0.0f ),
+    luminous::float4(  265.0f,  330.0f,  296.0f, 0.0f ),
+    luminous::float4(  265.0f,    0.0f,  296.0f, 0.0f ),
 
-    {  265.0f,    0.0f,  296.0f, 0.0f },
-    {  265.0f,  330.0f,  296.0f, 0.0f },
-    {  423.0f,  330.0f,  247.0f, 0.0f },
+    luminous::float4(  265.0f,    0.0f,  296.0f, 0.0f ),
+    luminous::float4(  265.0f,  330.0f,  296.0f, 0.0f ),
+    luminous::float4(  423.0f,  330.0f,  247.0f, 0.0f ),
 
-    {  265.0f,    0.0f,  296.0f, 0.0f },
-    {  423.0f,  330.0f,  247.0f, 0.0f },
-    {  423.0f,    0.0f,  247.0f, 0.0f },
+    luminous::float4(  265.0f,    0.0f,  296.0f, 0.0f ),
+    luminous::float4(  423.0f,  330.0f,  247.0f, 0.0f ),
+    luminous::float4(  423.0f,    0.0f,  247.0f, 0.0f ),
 
     // Ceiling light -- emmissive
-    {  343.0f,  548.6f,  227.0f, 0.0f },
-    {  213.0f,  548.6f,  227.0f, 0.0f },
-    {  213.0f,  548.6f,  332.0f, 0.0f },
+    luminous::float4(  343.0f,  548.6f,  227.0f, 0.0f ),
+    luminous::float4(  213.0f,  548.6f,  227.0f, 0.0f ),
+    luminous::float4(  213.0f,  548.6f,  332.0f, 0.0f ),
 
-    {  343.0f,  548.6f,  227.0f, 0.0f },
-    {  213.0f,  548.6f,  332.0f, 0.0f },
-    {  343.0f,  548.6f,  332.0f, 0.0f }
+    luminous::float4(  343.0f,  548.6f,  227.0f, 0.0f ),
+    luminous::float4(  213.0f,  548.6f,  332.0f, 0.0f ),
+    luminous::float4(  343.0f,  548.6f,  332.0f, 0.0f )
 } };
 
 static std::array<uint32_t, TRIANGLE_COUNT> g_mat_indices = {{
