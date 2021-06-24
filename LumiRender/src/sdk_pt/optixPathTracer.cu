f1524b3f6201bd5e2f83e52b2d6d849dadc913bb
Zero <249002031@qq.com>
2021-06-24 21:40:22 +0800

    Update optixPathTracer.cu

--- LumiRender/src/sdk_pt/optixPathTracer.cu
+++ LumiRender/src/sdk_pt/optixPathTracer.cu
@@ -114,33 +114,6 @@ static __forceinline__ __device__ void  packPointer( void* ptr, unsigned int& i0
 }
 
 
-static __forceinline__ __device__ RadiancePRD* getPRD()
-{
-    const unsigned int u0 = optixGetPayload_0();
-    const unsigned int u1 = optixGetPayload_1();
-    return reinterpret_cast<RadiancePRD*>( unpackPointer( u0, u1 ) );
-}
-
-
-static __forceinline__ __device__ void setPayloadOcclusion( bool occluded )
-{
-    optixSetPayload_0( static_cast<unsigned int>( occluded ) );
-}
-
-
-static __forceinline__ __device__ void cosine_sample_hemisphere(const float u1, const float u2, float3& p)
-{
-    // Uniformly sample disk.
-    const float r   = sqrtf( u1 );
-    const float phi = 2.0f*M_PIf * u2;
-    p.x = r * cosf( phi );
-    p.y = r * sinf( phi );
-
-    // Project up to hemisphere.
-    p.z = sqrtf( fmaxf( 0.0f, 1.0f - p.x*p.x - p.y*p.y ) );
-}
-
-
 static __forceinline__ __device__ void traceRadiance(
         OptixTraversableHandle handle,
         float3                 ray_origin,
@@ -210,7 +183,6 @@ extern "C" __global__ void __raygen__rg()
     prd.attenuation  = make_float3(1.f);
     prd.countEmitted = true;
     prd.done         = false;
-//        prd.seed         = seed;
     traceRadiance(
             params.traversable_handle,
             make_float3(278.0f, 273.0f, -900.0f),
@@ -218,167 +190,23 @@ extern "C" __global__ void __raygen__rg()
             0.01f,  // tmin       // TODO: smarter offset
             1e16f,  // tmax
             &prd );
-//    const int    w   = params.width;
-//    const int    h   = params.height;
-//    const float3 eye = params.eye;
-//    const float3 U   = params.U;
-//    const float3 V   = params.V;
-//    const float3 W   = params.W;
-//    const uint3  idx = optixGetLaunchIndex();
-//    const int    subframe_index = params.subframe_index;
-//
-//    unsigned int seed = tea<4>( idx.y*w + idx.x, subframe_index );
-//
-//    float3 result = make_float3( 0.0f );
-//    int i = params.samples_per_launch;
-//    do
-//    {
-//        // The center of each pixel is at fraction (0.5,0.5)
-//        const float2 subpixel_jitter = make_float2( rnd( seed ), rnd( seed ) );
-//
-//        const float2 d = 2.0f * make_float2(
-//                ( static_cast<float>( idx.x ) + subpixel_jitter.x ) / static_cast<float>( w ),
-//                ( static_cast<float>( idx.y ) + subpixel_jitter.y ) / static_cast<float>( h )
-//                ) - 1.0f;
-//        float3 ray_direction = normalize(d.x*U + d.y*V + W);
-//        float3 ray_origin    = eye;
-//
-//        RadiancePRD prd;
-//        prd.emitted      = make_float3(0.f);
-//        prd.radiance     = make_float3(0.f);
-//        prd.attenuation  = make_float3(1.f);
-//        prd.countEmitted = true;
-//        prd.done         = false;
-//        prd.seed         = seed;
-//
-//        int depth = 0;
-//        for( ;; )
-//        {
-//            traceRadiance(
-//                    params.handle,
-//                    ray_origin,
-//                    ray_direction,
-//                    0.01f,  // tmin       // TODO: smarter offset
-//                    1e16f,  // tmax
-//                    &prd );
-//
-//            result += prd.emitted;
-//            result += prd.radiance * prd.attenuation;
-//
-//            if( prd.done  || depth >= 5 ) // TODO RR, variable for depth
-//                break;
-//
-//            ray_origin    = prd.origin;
-//            ray_direction = prd.direction;
-//
-//            ++depth;
-//        }
-//    }
-//    while( --i );
-//
-//    const uint3    launch_index = optixGetLaunchIndex();
-//    const unsigned int image_index  = launch_index.y * params.width + launch_index.x;
-//    float3         accum_color  = result / static_cast<float>( params.samples_per_launch );
-//
-//    if( subframe_index > 0 )
-//    {
-//        const float                 a = 1.0f / static_cast<float>( subframe_index+1 );
-//        const float3 accum_color_prev = make_float3( params.accum_buffer[ image_index ]);
-//        accum_color = lerp( accum_color_prev, accum_color, a );
-//    }
-//    params.accum_buffer[ image_index ] = make_float4( accum_color, 1.0f);
-//    params.frame_buffer[ image_index ] = make_color ( accum_color );
+
 }
 
 
 extern "C" __global__ void __miss__radiance()
 {
-    MissData* rt_data  = reinterpret_cast<MissData*>( optixGetSbtDataPointer() );
-    RadiancePRD* prd = getPRD();
 
-    prd->radiance = make_float3( rt_data->bg_color );
-    prd->done      = true;
 }
 
 
 extern "C" __global__ void __closesthit__occlusion()
 {
-    setPayloadOcclusion( true );
+
 }
 
 
 extern "C" __global__ void __closesthit__radiance()
 {
-//    HitGroupData* rt_data = (HitGroupData*)optixGetSbtDataPointer();
-//
-//    const int    prim_idx        = optixGetPrimitiveIndex();
-//    printf("%d \n", prim_idx);
-//    const float3 ray_dir         = optixGetWorldRayDirection();
-//    const int    vert_idx_offset = prim_idx*3;
-//
-//    const float3 v0   = make_float3( rt_data->vertices[ vert_idx_offset+0 ] );
-//    const float3 v1   = make_float3( rt_data->vertices[ vert_idx_offset+1 ] );
-//    const float3 v2   = make_float3( rt_data->vertices[ vert_idx_offset+2 ] );
-//    const float3 N_0  = normalize( cross( v1-v0, v2-v0 ) );
-//
-//    const float3 N    = faceforward( N_0, -ray_dir, N_0 );
-//    const float3 P    = optixGetWorldRayOrigin() + optixGetRayTmax()*ray_dir;
-//
-//    RadiancePRD* prd = getPRD();
-//
-//    if( prd->countEmitted )
-//        prd->emitted = rt_data->emission_color;
-//    else
-//        prd->emitted = make_float3( 0.0f );
-//
-//
-//    unsigned int seed = prd->seed;
-//
-//    {
-//        const float z1 = rnd(seed);
-//        const float z2 = rnd(seed);
-//
-//        float3 w_in;
-//        cosine_sample_hemisphere( z1, z2, w_in );
-//        Onb onb( N );
-//        onb.inverse_transform( w_in );
-//        prd->direction = w_in;
-//        prd->origin    = P;
-//
-//        prd->attenuation *= rt_data->diffuse_color;
-//        prd->countEmitted = false;
-//    }
-//
-//    const float z1 = rnd(seed);
-//    const float z2 = rnd(seed);
-//    prd->seed = seed;
-//
-//    ParallelogramLight light = params.light;
-//    const float3 light_pos = light.corner + light.v1 * z1 + light.v2 * z2;
-//
-//    // Calculate properties of light sample (for area based pdf)
-//    const float  Ldist = length(light_pos - P );
-//    const float3 L     = normalize(light_pos - P );
-//    const float  nDl   = dot( N, L );
-//    const float  LnDl  = -dot( light.normal, L );
-//
-//    float weight = 0.0f;
-//    if( nDl > 0.0f && LnDl > 0.0f )
-//    {
-//        const bool occluded = traceOcclusion(
-//            params.handle,
-//            P,
-//            L,
-//            0.01f,         // tmin
-//            Ldist - 0.01f  // tmax
-//            );
-//
-//        if( !occluded )
-//        {
-//            const float A = length(cross(light.v1, light.v2));
-//            weight = nDl * LnDl * A / (M_PIf * Ldist * Ldist);
-//        }
-//    }
-//
-//    prd->radiance += light.emission * weight;
+
 }
