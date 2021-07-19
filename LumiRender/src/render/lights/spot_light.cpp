22b06d28a50d9de7747f7ea7cd0b9aed06163e2f
Zero <249002031@qq.com>
2021-07-19 15:07:11 +0800

    update spot

--- LumiRender/src/render/lights/spot_light.cpp
+++ LumiRender/src/render/lights/spot_light.cpp
@@ -8,5 +8,21 @@
 namespace luminous {
     inline namespace render {
 
+
+        Interaction SpotLight::sample(float u, const HitGroupData *hit_group_data) const {
+            return Interaction();
+        }
+
+        LightLiSample SpotLight::Li(LightLiSample lls) const {
+            return LightLiSample();
+        }
+
+        float SpotLight::PDF_Li(const Interaction &ref_p, const Interaction &p_light) const {
+            return 0;
+        }
+
+        float3 SpotLight::power() const {
+            return _intensity * 2.f * Pi * (1 - 0.5f * (_cos_theta_i + _cos_theta_o));
+        }
     } // luminous::render
 } // luminous
--- LumiRender/src/render/lights/spot_light.h
+++ LumiRender/src/render/lights/spot_light.h
@@ -1,43 +1,48 @@
-////
-//// Created by Zero on 2021/4/7.
-////
 //
+// Created by Zero on 2021/4/7.
 //
-//#pragma once
-//
-//#include "light.h"
-//#include "render/include/config.h"
-//
-//namespace luminous {
-//    inline namespace render {
-//
-//        class SpotLight : public LightBase {
-//        private:
-//            float3 _pos;
-//            float3 _axis;
-//            float _cos_theta_i;
-//            float _cos_theta_o;
-//            float3 _intensity;
-//        public:
-//            SpotLight(float3 pos, float3 intensity, float theta_i, float theta_o)
-//                    : LightBase(LightType::DeltaPosition),
-//                      _pos(pos),
-//                      _intensity(intensity),
-//                      _cos_theta_i(cos(radians(theta_i))),
-//                      _cos_theta_o(cos(radians(theta_o))) {}
-//
-//            NDSC_XPU Interaction sample(float u, const HitGroupData *hit_group_data) const;
-//
-//            NDSC_XPU LightLiSample Li(LightLiSample lls) const;
-//
-//            NDSC_XPU float PDF_Li(const Interaction &ref_p, const Interaction &p_light) const;
-//
-//            NDSC_XPU float3 power() const;
-//
-//            NDSC std::string to_string() const;
-//
-//            static SpotLight create(const LightConfig &config);
-//        };
-//
-//    } // luminous::render
-//} // luminous
+
+
+#pragma once
+
+#include "light.h"
+#include "render/include/config.h"
+
+namespace luminous {
+    inline namespace render {
+
+        class SpotLight : public LightBase {
+        private:
+            float3 _pos;
+            float3 _axis;
+            float _cos_theta_i;
+            float _cos_theta_o;
+            float3 _intensity;
+        public:
+            SpotLight(float3 pos, float3 intensity, float theta_i, float theta_o)
+                    : LightBase(LightType::DeltaPosition),
+                      _pos(pos),
+                      _intensity(intensity),
+                      _cos_theta_i(cos(radians(theta_i))),
+                      _cos_theta_o(cos(radians(theta_o))) {}
+
+            NDSC_XPU Interaction sample(float u, const HitGroupData *hit_group_data) const;
+
+            NDSC_XPU LightLiSample Li(LightLiSample lls) const;
+
+            NDSC_XPU float PDF_Li(const Interaction &ref_p, const Interaction &p_light) const;
+
+            NDSC_XPU float3 power() const;
+
+            GEN_STRING_FUNC({
+                LUMINOUS_TO_STRING("light Base : %s, name : %s ,intensity : %s",
+                                   LightBase::to_string().c_str(),
+                                   type_name(this),
+                                   _intensity.to_string().c_str());
+            })
+
+            CPU_ONLY(static PointLight create(const LightConfig &config);)
+        };
+
+    } // luminous::render
+} // luminous
