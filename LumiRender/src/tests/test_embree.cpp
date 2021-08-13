389ee3ff700213a273f1f60f6f398eeaca4eb6e1
zhuling1 <249002031@qq.com>
2021-08-13 15:20:45 +0800

    Update test_embree.cpp

--- LumiRender/src/tests/test_embree.cpp
+++ LumiRender/src/tests/test_embree.cpp
@@ -23,6 +23,7 @@ int main() {
     TriangleHandle tri[] = {{0,1,2},{3,1,2}};
     RTCScene rtc_scene = rtcNewScene(rtc_device);
     Transform transform = Transform::translation(make_float3(-1,0.0,0));
+    Transform transform2 = Transform::translation(make_float3(0,0.0,0));
 
     RTCGeometry rtc_geometry = rtcNewGeometry(rtc_device, RTC_GEOMETRY_TYPE_TRIANGLE);
 
@@ -37,23 +38,34 @@ int main() {
     rtcCommitGeometry(rtc_geometry);
 
     rtcAttachGeometry(rtc_scene, rtc_geometry);
+    rtcCommitScene(rtc_scene);
 
     RTCGeometry instance = rtcNewGeometry(rtc_device, RTC_GEOMETRY_TYPE_INSTANCE);
+    RTCGeometry instance2 = rtcNewGeometry(rtc_device, RTC_GEOMETRY_TYPE_INSTANCE);
     rtcSetGeometryInstancedScene(instance, rtc_scene);
     rtcSetGeometryTransform(instance, 0, RTC_FORMAT_FLOAT4X4_COLUMN_MAJOR, transform.mat4x4_ptr());
+
+    rtcSetGeometryInstancedScene(instance2, rtc_scene);
+    rtcSetGeometryTransform(instance2, 0, RTC_FORMAT_FLOAT4X4_COLUMN_MAJOR, transform2.mat4x4_ptr());
+
     rtcCommitGeometry(instance);
-    rtcCommitScene(rtc_scene);
+    rtcCommitGeometry(instance2);
+
     RTCScene main_scene = rtcNewScene(rtc_device);
-    rtcAttachGeometry(main_scene, rtc_geometry);
     rtcAttachGeometry(main_scene, instance);
+//    rtcAttachGeometry(main_scene, instance2);
+
 
     rtcCommitScene(main_scene);
 
-    Ray ray(make_float3(0.1, 0.5, 1), make_float3(0,0,-1));
+    Ray ray(make_float3(-0.9, 0.3, 1), make_float3(0,0,-1));
     PerRayData prd;
 
     Clock clk;
     intersect_closest((uint64_t)main_scene, ray, &prd);
+
+    RTCBounds rb;
+    rtcGetSceneBounds(main_scene, &rb);
 
     auto t = clk.elapse_ms();
     prd.closest_hit.print();
