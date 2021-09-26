6e92641ea7e40ee4a6897466be1f4282a7fba366
zhuling1 <249002031@qq.com>
2021-09-26 14:44:53 +0800

    简化测试代码

--- LumiRender/src/tests/test_light.h
+++ LumiRender/src/tests/test_light.h
@@ -5,89 +5,34 @@
 
 #pragma once
 
-#include "render/lights/light.h"
-
 namespace luminous {
     inline namespace render {
 
-        template<typename...T>
-        struct BaseBinder2 : public T ... {
-            using Bases = std::tuple<T...>;
+        struct BaseBinder3 {
 
-            static constexpr auto base_num = std::tuple_size_v<Bases>;
+            BaseBinder3() = default;
 
-            template<int idx>
-            using Base = std::tuple_element_t<idx, Bases>;
-
-            BaseBinder2() = default;
-
-            explicit BaseBinder2(T &&...args)
-                    : T{std::move(args)}... {}
         };
 
-        template<>
-        struct BaseBinder2<> {
-            using Bases = std::tuple<>;
-
-            static constexpr auto base_num = std::tuple_size_v<Bases>;
-
-            template<int idx>
-            using Base = void;
-
-            BaseBinder2() = default;
-        };
-
-        template<typename T>
-        struct BaseBinder2<T> : public T {
-            using Bases = std::tuple<T>;
-
-            static constexpr auto base_num = std::tuple_size_v<Bases>;
-
-            template<int idx>
-            using Base = T;
-
-            BaseBinder2() = default;
-
-            using T::T;
-
-            explicit BaseBinder2(T &&t)
-                    : T{std::move(t)} {}
-        };
-
-
-        struct LB : BaseBinder2<> {
+        struct LB : BaseBinder3 {
         public:
-//            REFL_CLASS(LB)
-        public:
-            const LightType _type{};
-        public:
+            int _type{};
             LB()
-                    : _type(LightType::Area) {}
+                    : _type(0) {}
         };
 
-        template<typename T>
         struct ICreator2 {
         public:
-            CPU_ONLY(
-                    template<typename ...Args>
-                    static T create(Args &&...args) {
-                        return T(std::forward<Args>(args)...);
-                    }
-            )
-
-            CPU_ONLY(
-                    template<typename ...Args>
-                    static T *create_ptr(Args &&...args) {
-                        return new T(std::forward<Args>(args)...);
-                    }
-            )
         };
 
-        struct AL : public LB, public luminous::ICreator2<AL> {
+        struct AL : public LB, public luminous::ICreator2 {
         public:
-            float padded{};
+            float padded{9};
+            float paohui{};
 
-            AL() : LB() {}
+            AL() : LB() {
+                padded = 10;
+            }
         };
     }
 }
