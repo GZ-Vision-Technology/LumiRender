e7b0e1746f5c245bc4aeb4ae0891e0cb2b3469e3
zhuling1 <249002031@qq.com>
2021-10-15 17:03:51 +0800

    Update play_ground.cpp

--- LumiRender/src/tests/play_ground.cpp
+++ LumiRender/src/tests/play_ground.cpp
@@ -6,26 +6,55 @@
 #include <tuple>
 #include <utility>
 
+using namespace std;
+
+template<typename func_type>
+class Kernel {
+protected:
+    func_type _func{};
+public:
+    explicit Kernel(func_type func) : _func(func) {}
+
+    template<typename Ret, typename...Args, size_t...Is>
+    void call_impl(Ret(*f)(Args...), void *args[], std::index_sequence<Is...>) {
+        f(*static_cast<std::tuple_element_t<Is, std::tuple<Args...>> *>(args[Is])...);
+    }
+
+    template<typename Ret, typename...Args>
+    void call(Ret(*f)(Args...), void *args[]) {
+        call_impl(f, args, std::make_index_sequence<sizeof...(Args)>());
+    }
+
+    template<typename...Args>
+    void launch(Args &...args) {
+        void *array[]{(&args)...};
+        call(_func, array);
+    }
+};
+
 template<typename Ret, typename...Args, size_t...Is>
-void call_impl(Ret(*f)(Args...), void *args[], std::index_sequence<Is...>)
-{
-    f(*static_cast<std::tuple_element_t<Is, std::tuple<Args...>>*>(args[Is])...);
+void call_impl(Ret(*f)(Args...), void *args[], std::index_sequence<Is...>) {
+    f(*static_cast<std::tuple_element_t<Is, std::tuple<Args...>> *>(args[Is])...);
 }
 
 template<typename Ret, typename...Args>
-void call(Ret(*f)(Args...), void *args[])
-{
+void call(Ret(*f)(Args...), void *args[]) {
     call_impl(f, args, std::make_index_sequence<sizeof...(Args)>());
 }
 
-void foo(int x, float y)
-{
+void foo(int x, float y) {
     std::cout << x << " " << y << std::endl;
 }
 
-int main()
-{
-    int x = 4; float y = 6.5f;
-    void *args[] = { &x, &y };
+int main() {
+    int x = 4;
+    float y = 6.5f;
+    void *args[] = {&x, &y};
     call(foo, args);
+
+    Kernel kernel(foo);
+
+    cout << typeid(foo).name() << endl;
+
+    kernel.launch(x, y);
 }
