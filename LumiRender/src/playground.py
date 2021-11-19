0d11a28663daac16a121b09947bf9bb4dd68be5f
zhuling1 <249002031@qq.com>
2021-11-19 18:38:04 +0800

    矩阵提取欧拉角

--- LumiRender/src/playground.py
+++ LumiRender/src/playground.py
@@ -4,62 +4,6 @@ import os
 import numpy as np
 import math
 
-m1 = np.array([
-    [1,2],
-    [3,4]
-])
-
-
-m2 = np.array([
-    [2,1],
-    [4,3]
-])
-
-print(np.matmul(m1, m2))
-print(np.matmul(m2, m1))
-
-# count = 0
-
-# com = 0
-
-# inCom = False
-
-# num_file = 0
-
-# for root,dirs,files in os.walk(os.path.join(os.getcwd(), "LumiRender/src")):
-#     for file in files:
-#         fn = os.path.join(root,file)
-#         if "ext\\" in fn:
-#             continue
-#         if "gui" in fn:
-#             continue
-#         if "stats.py" in fn:
-#             continue
-#         if "jitify" in fn:
-#             continue
-#         if "sdk_pt" in fn:
-#             continue
-#         try:
-#             f = open(fn, "r")
-#             count += len(f.readlines())
-#         except :
-#             print(fn)
-
-        
-        
-#         num_file += 1
-
-
-# print(count, num_file)
-        
-# class C:
-#     pass
-# class A:
-#     pass
-# class B(A, C):
-#     pass
-    
-# print(issubclass(B,C))  
 
 def rotation_x(theta):
     theta = math.radians(theta)
@@ -99,27 +43,35 @@ class Camera:
     
     def update(self, m):
         sy = math.sqrt(sqr(m[2][1]) + sqr(m[2][2]))
-        self.pitch = math.degrees(math.atan2(m[2][1], abs(m[2][2])))
-        self.yaw = math.degrees(-math.atan2(-m[2][0], sy))
+        # self.pitch = math.degrees(math.atan2(m[2][1], abs(m[2][2])))
+        # self.yaw = math.degrees(-math.atan2(-m[2][0], sy))
         
-    def camera_to_world_rotation():
-        horizontal = rotation_y(yaw);
-        vertical = rotation_x(-pitch);
+        self.pitch = -math.degrees(math.atan2(m[1][2], (m[1][1])))
+        self.yaw = math.degrees(math.atan2(m[2][0], m[0][0]))
+        
+        # print("-----------------------", (m[2][0], m[0][0]))
+        
+    def camera_to_world_rotation(self):
+        horizontal = rotation_y(self.yaw);
+        vertical = rotation_x(-self.pitch);
         return np.matmul(horizontal, vertical)
     
     def __repr__(self) -> str:
         return "yaw:%s, pitch: %s" % (self.yaw, self.pitch)
 
 
-yaw = 95
+yaw = -150
 pitch = 20
 
 mat = view_mat(pitch, yaw)
 
-# print(rotation_x(pitch))
+# print(rotation_x(-pitch))
 # print(rotation_y(yaw))
+print(mat)
 
 cam = Camera(mat)
 
 print(cam)
+
+print(cam.camera_to_world_rotation())
 
