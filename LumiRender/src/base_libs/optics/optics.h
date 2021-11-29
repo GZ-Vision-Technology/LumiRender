42c353b262900152fac7e260e2bcb7d180498216
zhuling1 <249002031@qq.com>
2021-11-29 18:33:34 +0800

    Update optics.h

--- LumiRender/src/base_libs/optics/optics.h
+++ LumiRender/src/base_libs/optics/optics.h
@@ -40,5 +40,52 @@ namespace luminous {
         ND_XPU_INLINE float schlick_R0_from_eta(float eta) {
             return sqr(eta - 1) / sqr(eta + 1);
         }
+
+        ND_XPU_INLINE float henyey_greenstein(float cos_theta, float g) {
+            float denom = 1 + sqr(g) + 2 * g * cos_theta;
+            return inv4Pi * (1 - sqr(g)) / (denom * safe_sqrt(denom));
+        }
+
+        ND_XPU_INLINE float fresnel_dielectric(float cosTheta_i, float eta) {
+            cosTheta_i = clamp(cosTheta_i, -1, 1);
+            if (cosTheta_i < 0) {
+                eta = 1 / eta;
+                cosTheta_i = -cosTheta_i;
+            }
+
+            float sin2Theta_i = 1 - sqr(cosTheta_i);
+            float sin2Theta_t = sin2Theta_i / sqr(eta);
+            if (sin2Theta_t >= 1)
+                return 1.f;
+            float cosTheta_t = safe_sqrt(1 - sin2Theta_t);
+
+            float r_parl = (eta * cosTheta_i - cosTheta_t) / (eta * cosTheta_i + cosTheta_t);
+            float r_perp = (cosTheta_i - eta * cosTheta_t) / (cosTheta_i + eta * cosTheta_t);
+            return (r_parl * r_parl + r_perp * r_perp) / 2;
+        }
+
+        ND_XPU_INLINE Spectrum fresnel_conductor(float cos_theta_i, const Spectrum &eta_i,
+                                                 const Spectrum &eta_t, const Spectrum &kt) {
+            cos_theta_i = clamp(cos_theta_i, -1, 1);
+            Spectrum eta = eta_t / eta_i;
+            Spectrum eta_k = kt / eta_i;
+
+            float cos_theta_i_2 = sqr(cos_theta_i);
+            float sin_theta_i_2 = 1.f - cos_theta_i_2;
+
+            Spectrum t0 = sqr(eta) - sqr(eta_k) - sin_theta_i_2;
+            Spectrum a2plusb2 = sqrt(sqr(t0) + 4.f * sqr(eta) * sqr(eta_k));
+            Spectrum t1 = a2plusb2 + cos_theta_i_2;
+            Spectrum a = sqrt(0.5f * (a2plusb2 + t0));
+            Spectrum t2 = 2.f * cos_theta_i * a;
+            Spectrum Rs = (t1 - t2) / (t1 + t2);
+
+            Spectrum t3 = cos_theta_i_2 * a2plusb2 + sqr(sin_theta_i_2);
+            Spectrum t4 = t2 * sin_theta_i_2;
+            Spectrum Rp = Rs * (t3 - t4) / (t3 + t4);
+
+            return 0.5f * (Rp + Rs);
+        }
+
     }
 }
