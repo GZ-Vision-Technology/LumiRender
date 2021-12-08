1d9a27d5cee63fe90bea0813ae3cbe64b7925846
zhuling1 <249002031@qq.com>
2021-12-08 10:58:48 +0800

    删除测试代码

--- LumiRender/src/render/samplers/sampler.h
+++ LumiRender/src/render/samplers/sampler.h
@@ -17,7 +17,7 @@ namespace luminous {
 
         class Filter;
 
-        class Sampler : BASE_CLASS(Variant<LCGSampler, LCGSampler,LCGSampler,LCGSampler,LCGSampler,LCGSampler, PCGSampler>) {
+        class Sampler : BASE_CLASS(Variant<LCGSampler, PCGSampler>) {
         public:
             REFL_CLASS(Sampler)
 
