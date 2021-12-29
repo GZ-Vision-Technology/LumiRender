73e0821ca5b9c0881de5bc7532c52b41f555a45c
zhuling1 <249002031@qq.com>
2021-12-29 21:42:47 +0800

    Update disney_bsdf.h

--- LumiRender/src/render/scattering/disney_bsdf.h
+++ LumiRender/src/render/scattering/disney_bsdf.h
@@ -14,24 +14,41 @@ namespace luminous {
         namespace disney {
             class Diffuse : public BxDF<Diffuse> {
             private:
-                float _factor{};
+                float _weight{};
             public:
                 using BxDF::BxDF;
 
-                LM_XPU explicit Diffuse(float factor) : _factor(factor) {}
+                LM_XPU explicit Diffuse(float weight) : _weight(weight) {}
 
                 LM_ND_XPU Spectrum eval(float3 wo, float3 wi, BSDFParam data,
                                         TransportMode mode = TransportMode::Radiance) const {
                     float Fo = schlick_weight(Frame::abs_cos_theta(wo));
                     float Fi = schlick_weight(Frame::abs_cos_theta(wi));
-                    return _factor * data.color() * invPi * (1 - Fo / 2) * (1 - Fi / 2);
+                    return _weight * data.color() * invPi * (1 - Fo / 2) * (1 - Fi / 2);
                 }
             };
 
             class FakeSS : public BxDF<FakeSS> {
             private:
                 float _roughness{};
-                float _factor{};
+                float _weight{};
+            };
+
+            class Retro : public BxDF<Retro> {
+            private:
+                float _roughness{};
+                float _weight{};
+            };
+
+            class Sheen : public BxDF<Sheen> {
+            private:
+                float _weight{};
+            };
+
+            class Clearcoat : public BxDF<Clearcoat> {
+            private:
+                float _weight{};
+                float _gloss{};
             };
         }
     }
