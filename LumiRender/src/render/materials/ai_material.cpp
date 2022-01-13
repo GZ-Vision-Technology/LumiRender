20609edb918b86184e545ed905d869010a772f33
zhuling1 <249002031@qq.com>
2022-01-13 18:13:50 +0800

    Update ai_material.cpp

--- LumiRender/src/render/materials/ai_material.cpp
+++ LumiRender/src/render/materials/ai_material.cpp
@@ -10,8 +10,11 @@ namespace luminous {
 
         BSDFWrapper AssimpMaterial::get_BSDF(const MaterialEvalContext &ctx, const SceneData *scene_data) const {
             float4 color = scene_data->get_texture(_color_idx).eval(ctx);
-            DiffuseBSDF diffuse_bsdf = create_diffuse_bsdf(color);
-            BSDF bsdf{diffuse_bsdf};
+//            DiffuseBSDF diffuse_bsdf = create_diffuse_bsdf(color);
+//            BSDF bsdf{diffuse_bsdf};
+            DisneyBSDFData data;
+            data.color = color;
+            BSDF bsdf{data.create()};
             return {ctx.ng, ctx.ns, ctx.dp_dus, bsdf};
         }
     }
