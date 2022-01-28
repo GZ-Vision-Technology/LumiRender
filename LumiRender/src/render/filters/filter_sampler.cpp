d5f78648c685324063b03cd8e29cb3b67c87d6d5
zhuling1 <249002031@qq.com>
2022-01-28 17:16:29 +0800

    优化fis

--- LumiRender/src/render/filters/filter_sampler.cpp
+++ LumiRender/src/render/filters/filter_sampler.cpp
@@ -34,8 +34,10 @@ namespace luminous {
             float PDF = 0;
             int2 offset{};
             float2 p = _distribution2d.sample_continuous(abs(u), &PDF, &offset);
+            float w = _distribution2d.func_at(offset);
             p = p * sign(u);
-            return FilterSample{p, 1.f * _signs(offset)};
+            // todo implement alias table sampling
+            return FilterSample{p, 1};
         }
     }
 }
--- LumiRender/src/render/sensors/film.cpp
+++ LumiRender/src/render/sensors/film.cpp
@@ -22,7 +22,7 @@ namespace luminous {
                     if (val.w == 0) {
                         return;
                     }
-                    float3 color = make_float3(val) / val.w;
+                    float3 color = make_float3(val);
                     // float4 framebuffer lead performance reduction about 10%
                     fill_frame_buffer_pixel_loc(Spectrum::linear_to_srgb(color), _frame_buffer_view[pixel_index]);
                     break;
@@ -32,7 +32,7 @@ namespace luminous {
                     if (val.w == 0) {
                         return;
                     }
-                    float3 color = make_float3(val) / val.w;
+                    float3 color = make_float3(val);
                     fill_frame_buffer_pixel_loc(Spectrum::linear_to_srgb(color), _frame_buffer_view[pixel_index]);
                     break;
                 }
@@ -41,7 +41,7 @@ namespace luminous {
                     if (val.w == 0) {
                         return;
                     }
-                    float3 normal = make_float3(val) / val.w;
+                    float3 normal = make_float3(val);
                     fill_frame_buffer_pixel_loc((normal + 1.f) / 2.f, _frame_buffer_view[pixel_index]);
                     break;
                 }
@@ -55,14 +55,12 @@ namespace luminous {
                                uint frame_index, BufferView<float4> buffer_view) {
             val = val * weight;
             DCHECK(!has_invalid(val));
-            if (frame_index == 0) {
-                buffer_view[pixel_index] = make_float4(val, weight);
-            } else {
-                float pre_weight_sum = buffer_view[pixel_index].w;
-                // todo float precision
-                const float3 accum_val_prev = make_float3(buffer_view[pixel_index]);
-                buffer_view[pixel_index] = make_float4(val + accum_val_prev, pre_weight_sum + weight);
+            if (frame_index > 0) {
+                const float a = 1.0f / static_cast<float>(frame_index + 1);
+                const float3 accum_color_prev = make_float3(buffer_view[pixel_index]);
+                val = lerp(a, accum_color_prev, val);
             }
+            buffer_view[pixel_index] = make_float4(val, 1.f);
         }
 
         void Film::_update() {
