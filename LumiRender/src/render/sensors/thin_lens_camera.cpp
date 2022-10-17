3b79de63335cff8ad25eac24c436ff2a3b942d7e
zhuling1 <249002031@qq.com>
2022-10-17 23:36:41 +0800

    Update thin_lens_camera.cpp

--- LumiRender/src/render/sensors/thin_lens_camera.cpp
+++ LumiRender/src/render/sensors/thin_lens_camera.cpp
@@ -17,23 +17,33 @@ namespace luminous {
                   _lens_radius(lens_radius),
                   _focal_distance(focal_distance) {}
 
+        // float ThinLensCamera::generate_ray(const SensorSample &ss, Ray *ray) {
+        //     float3 p_film = make_float3(ss.p_film, 0);
+        //     float3 p_sensor = _raster_to_camera.apply_point(p_film);
+        //     auto c2w = camera_to_world();
+
+        //     *ray = {make_float3(0, 0, 0), normalize(p_sensor)};
+        //     if (_lens_radius > 0) {
+        //         float2 p_lens = _lens_radius * square_to_disk(ss.p_lens);
+
+        //         // Compute point on plane of focus
+        //         float ft = _focal_distance / ray->dir_z;
+        //         float3 p_focus = ray->at(ft);
+
+        //         ray->update_origin(make_float3(p_lens, 0));
+        //         ray->update_direction(normalize(p_focus - ray->origin()));
+        //     }
+        //     *ray = c2w.apply_ray(*ray);
+        //     return 1 * ss.filter_weight;
+        // }
+
         float ThinLensCamera::generate_ray(const SensorSample &ss, Ray *ray) {
-            float3 p_film = make_float3(ss.p_film, 0);
-            float3 p_sensor = _raster_to_camera.apply_point(p_film);
-            auto c2w = camera_to_world();
-
-            *ray = {make_float3(0, 0, 0), normalize(p_sensor)};
-            if (_lens_radius > 0) {
-                float2 p_lens = _lens_radius * square_to_disk(ss.p_lens);
-
-                // Compute point on plane of focus
-                float ft = _focal_distance / ray->dir_z;
-                float3 p_focus = ray->at(ft);
-
-                ray->update_origin(make_float3(p_lens, 0));
-                ray->update_direction(normalize(p_focus - ray->origin()));
-            }
-            *ray = c2w.apply_ray(*ray);
+            uint2 res = resolution();
+            float2 p = (ss.p_film * 2.0f - float2(res)) * (std::tan(radians(_fov_y) * 0.5f) / res.y);
+            auto direction = normalize(p.x * make_float3(camera_to_world().mat4x4()[0]) - p.y * make_float3(camera_to_world().mat4x4()[1]) + make_float3(camera_to_world().mat4x4()[2]));
+            p.y *= -1;
+            direction = camera_to_world_rotation().apply_vector(make_float3(p, 1));
+            *ray = {position(), direction};
             return 1 * ss.filter_weight;
         }
     }
